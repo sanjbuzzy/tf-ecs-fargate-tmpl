5503e6a334f9a4ffbf05d7baf68aaf537a7cd6d3
Sanj Edirisinghe <sanj.edirisinghe@buzzy.buzz>
2021-08-29 18:54:19 +1000

    changed default region refs

--- variables.tf
+++ variables.tf
@@ -34,7 +34,7 @@ variable "aws-secret-key" {
 
 variable "availability_zones" {
   description = "a comma-separated list of availability zones, defaults to all AZ of the region, if set to something other than the defaults, both private_subnets and public_subnets have to be defined as well"
-  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  default     = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
 }
 
 variable "cidr" {
